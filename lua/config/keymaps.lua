3ce3e3cd82efda878747acb074bbcd4ca1daca69
Juliusz Sadowski <sjulek6011@gmail.com>
2026-01-04 23:12:57 +0100

    added some descriptions to custom keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,9 @@
 local km = vim.keymap
 local opts = { noremap = true, silent = true }
 
-km.set("n", "<C-d>", "<C-d>zz", opts)
-km.set("v", "<C-d>", "<C-d>zz", opts)
-km.set("v", "<C-u>", "<C-u>zz", opts)
-km.set("n", "<C-u>", "<C-u>zz", opts)
+km.set("n", "<C-d>", "<C-d>zz", vim.tbl_extend("force", opts, { desc = "Scroll half down (centered)" }))
+km.set("v", "<C-d>", "<C-d>zz", vim.tbl_extend("force", opts, { desc = "Scroll half down (centered)" }))
+km.set("v", "<C-u>", "<C-u>zz", vim.tbl_extend("force", opts, { desc = "Scroll half up (centered)" }))
+km.set("n", "<C-u>", "<C-u>zz", vim.tbl_extend("force", opts, { desc = "Scroll half up (centered)" }))
+km.set("n", "d", '"_d', opts)
+km.set("v", "d", '"_d', opts)
