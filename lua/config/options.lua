af0984ef9007772c0b4ec3dfc98ad512e853e991
Juliusz Sadowski <sjulek6011@gmail.com>
2026-01-11 19:57:28 +0100

    powershell as default for windows maybe

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+if LazyVim.is_win() then
+  if vim.fn.executable("pwsh") == 1 then
+    LazyVim.terminal.setup("pwsh")
+  elseif vim.fn.executable("powershell") == 1 then
+    LazyVim.terminal.setup("powershell")
+  end
+end
