3ad339cae169586ad59e881bad3de4f8cf0e465e
Juliusz Sadowski <sjulek6011@gmail.com>
2026-01-16 12:26:14 +0100

    changed keymaps so caps is no longer esc :(

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,5 +10,6 @@ km.set("v", "<c-d>", "<c-d>zz", vim.tbl_extend("force", opts, { desc = "Scroll h
 km.set("v", "<c-u>", "<c-u>zz", vim.tbl_extend("force", opts, { desc = "Scroll half up (centered)" }))
 km.set("n", "<c-u>", "<c-u>zz", vim.tbl_extend("force", opts, { desc = "Scroll half up (centered)" }))
 --#endregion
-km.set("t", "<Esc><Esc>", [[<C-\><C-n>]], opts)
+km.set("t", "<Esc><Esc>", [[<C-\><C-n>]], vim.tbl_extend("force", opts, { desc = "Exit terminal mode" }))
+km.set("t", "jk", [[<C-\><C-n>]], vim.tbl_extend("force", opts, { desc = "Exit terminal mode" }))
 km.set("i", "jk", "<Esc>", opts)
