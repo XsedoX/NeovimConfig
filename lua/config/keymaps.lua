026fc79e02782713053f3c3cf57a3b4ccf06214f
Juliusz Sadowski <sjulek6011@gmail.com>
2026-01-20 13:16:56 +0100

    added centering to capital g

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,8 @@ km.set("n", "<c-d>", "<c-d>zz", vim.tbl_extend("force", opts, { desc = "Scroll h
 km.set("v", "<c-d>", "<c-d>zz", vim.tbl_extend("force", opts, { desc = "Scroll half down (centered)" }))
 km.set("v", "<c-u>", "<c-u>zz", vim.tbl_extend("force", opts, { desc = "Scroll half up (centered)" }))
 km.set("n", "<c-u>", "<c-u>zz", vim.tbl_extend("force", opts, { desc = "Scroll half up (centered)" }))
+km.set("n", "G", "Gzz", vim.tbl_extend("force", opts, { desc = "Go to end of file (centered)" }))
+km.set("v", "G", "Gzz", vim.tbl_extend("force", opts, { desc = "Go to end of file (centered)" }))
 --#endregion
 km.set("t", "<Esc><Esc>", [[<C-\><C-n>]], vim.tbl_extend("force", opts, { desc = "Exit terminal mode" }))
 km.set("t", "jk", [[<C-\><C-n>]], vim.tbl_extend("force", opts, { desc = "Exit terminal mode" }))
