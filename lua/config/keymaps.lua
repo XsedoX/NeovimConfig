f61571b03f913bfc7098517db82aed8eb340be3d
Juliusz Sadowski <sjulek6011@gmail.com>
2026-01-22 13:22:04 +0100

    added jj as escape option

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,6 +12,10 @@ km.set("n", "<c-u>", "<c-u>zz", vim.tbl_extend("force", opts, { desc = "Scroll h
 km.set("n", "G", "Gzz", vim.tbl_extend("force", opts, { desc = "Go to end of file (centered)" }))
 km.set("v", "G", "Gzz", vim.tbl_extend("force", opts, { desc = "Go to end of file (centered)" }))
 --#endregion
+--#region escapes
 km.set("t", "<Esc><Esc>", [[<C-\><C-n>]], vim.tbl_extend("force", opts, { desc = "Exit terminal mode" }))
 km.set("t", "jk", [[<C-\><C-n>]], vim.tbl_extend("force", opts, { desc = "Exit terminal mode" }))
 km.set("i", "jk", "<Esc>", opts)
+km.set("t", "jj", [[<C-\><C-n>]], vim.tbl_extend("force", opts, { desc = "Exit terminal mode" }))
+km.set("i", "jj", "<Esc>", opts)
+--#endregion
