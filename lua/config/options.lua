e3e27de6a8189da4e2c05a8842016387a2fc8fba
Juliusz Sadowski <sjulek6011@gmail.com>
2026-07-15 13:23:00 +0200

    fixed copy-paste with wezterm

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,3 +10,24 @@ if LazyVim.is_win() then
 end
 vim.g.root_spec = { "cwd" }
 vim.opt.title = true
+
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
