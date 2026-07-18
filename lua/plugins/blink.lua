d904290fd1c0489c58dbd1afb8a7c90a4734ea80
Juliusz Sadowski <93394177+XsedoX@users.noreply.github.com>
2026-07-18 20:06:16 +0200

    Update blink.lua

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,8 +1,11 @@
 return {
   "saghen/blink.cmp",
-  opts = {
-    keymap = {
+  opts = function(_, opts)
+    opts.keymap = {
+      preset = false, -- disable preset so it can't rebind Tab
       ["<Tab>"] = false,
-    },
-  },
+      ["<S-Tab>"] = false,
+    }
+    return opts
+  end,
 }
